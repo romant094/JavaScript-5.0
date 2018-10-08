41146a9cecda7783b81993c49e7d0b9723c15b18
Anton <ar@asque.me>
2018-10-09 00:34:49 +0300

    added lesson 3

--- Common/js/script.js
+++ Common/js/script.js
@@ -1,7 +1,17 @@
 'use strict';
 
-let money = +prompt("Ваш бюджет на месяц?", ""),
+let money,
+    time;
+
+function start() {
+    while (isNaN(money) || money == '' || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
+}
+
+start();
 
 let appData = {
     budget: money,
@@ -9,57 +19,64 @@ let appData = {
     optionalExpenses: {},
     income: [],
     timeData: time,
-    savings: false
+    savings: true
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt("Во сколько обойдется?", "");
-    if ((typeof (a) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50)) {
-        console.log('done');
-        appData.expenses[a] = b;
-    } else {
-        i--;
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt("Во сколько обойдется?", "");
+        if ((typeof (a) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50)) {
+            console.log('done');
+            appData.expenses[a] = b;
+        } else {
+            i--;
+        }
     }
 }
 
-// Цикл while
-// let i = 0;
-// while (i < 2) {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt("Во сколько обойдется?", "");
-//     if ((typeof (a) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50)) {
-//         console.log('done');
-//         appData.expenses[a] = b;
-//         i++;
-//     }
-// }
-// =====================================
+chooseExpenses();
 
-// Цикл do while
-// let i = 0;
-// do {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt("Во сколько обойдется?", "");
-//     if ((typeof (a) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50)) {
-//         console.log('done');
-//         appData.expenses[a] = b;
-//         i++;
-//     }
-// }
-// while (i < 2);
-// =====================================
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+}
 
-appData.moneyPerDay = appData.budget / 30;
+detectDayBudget();
 
 alert("Ваш бюджет на 1 день: " + appData.moneyPerDay);
 
-if (appData.moneyPerDay < 100) {
-    console.log('Минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('Средний уровень достатка');
-} else if (appData.moneyPerDay > 2000) {
-    console.log('Высокий уровень достатка');
-} else {
-    console.log('Произошла ошибка');
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log('Минимальный уровень достатка');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log('Средний уровень достатка');
+    } else if (appData.moneyPerDay > 2000) {
+        console.log('Высокий уровень достатка');
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
+
+detectLevel();
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt('Какова сумма накоплений?'),
+            percent = +prompt('Под какой процент?');
+
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert('Доход с Вашего депозита в месяц: ' + appData.monthIncome);
+    }
+}
+
+checkSavings();
+
+function chooseOptExpenses() {
+    let optExpenses;
+    for (let i = 0; i < 3; i++){
+        optExpenses = prompt('Статья необязательных расходов?');
+        appData.optionalExpenses[i + 1] = optExpenses;
+    }
+}
+
+chooseOptExpenses();
---
+++ extra/lesson_3/js/lesson3.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function calc() {
+    let x = 50;
+    return x;
+}
+
+let num = calc(),
+    num1 = x;
+
+
+console.log(0 || "" || 2 || undefined || true || falsе);
