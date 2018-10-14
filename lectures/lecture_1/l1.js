e6ea995cd94a591ff6368ee06512fcce9d34e9a5
Anton <ar@asque.me>
2018-10-14 10:41:53 +0300

    added lesson 5 easy task part 2

--- lectures/lecture_1/l1.js
+++ lectures/lecture_1/l1.js
@@ -12,6 +12,6 @@ for (let i = 1; i <= 100; i++) {
     }
 }
 
-obj.simple.forEach(function (elem, i) {
+obj.simple.forEach(function(elem, i) {
     if (elem.length < 3) document.write(`<p>Простое число ${i}, делители ${elem}</p>`);
 });
