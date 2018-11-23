3beef6f05b19f5095360e2d6ae2865687b498ab5
Anton Romankov <ar@asque.me>
2018-11-23 13:58:08 +0300

    New algorythm in lesson 4 extra

--- extra/lesson_4/exam/index.js
+++ extra/lesson_4/exam/index.js
@@ -1,68 +1,95 @@
+let startInput = document.querySelector('#start'),
+	endInput = document.querySelector('#end'),
+	findFriendly = document.querySelector('#find-friendly');
+
 function getFriendlyNumbers(start, end) {
-
-    // if ((typeof (parseFloat(start)) === 'string') || (typeof (parseFloat(end)) === 'string') || (typeof (start) == null) || (typeof (end) == null) || (start = '') || (end = '') || (end > start) || (start < 0) || (end < 0)) {
-    //     return false;
-    // } else {
-    let arr = [];
-
-    for (let i = start; i <= end; i++) {
-        arr.push(i);
-    }
-
-    return arr;
-}
-// }
-
-console.log(getFriendlyNumbers(200, 300));
-
-function findFriendly(num) {
-    let obj = {
-        arr1: [],
-        sum1: 0,
-        arr2: [],
-        sum2: 0
-    };
-
-    if (num > 3) {
-        for (let i = 0; i < num; i++) {
-            if (num % i === 0) {
-                obj.arr1.push(i);
-                obj.sum1 += i;
-            }
-        }
-
-        console.log(`arr1: ${obj.arr1}`);
-        console.log(`sum1: ${obj.sum1}`);
-
-        for (let i = 0; i < obj.sum1; i++) {
-            if (obj.sum1 % i === 0) {
-                obj.arr2.push(i);
-                obj.sum2 += i;
-            }
-        }
-        console.log(`arr2: ${obj.arr2}`);
-        console.log(`sum2: ${obj.sum2}`);
-
-        (obj.sum2 === num) ? console.log(`Для числа ${num} есть дружественное число: ${obj.sum1}`) : console.log(`Дружественного числа не найдено`);
-    } else {
-        alert(`Введите число больше 3! `);
-    }
+	start = parseInt(startInput.value);
+	end = parseInt(endInput.value);
+	
+	if (validation(start, end)) {
+		let arr = createArray(start, end),
+			res = [];
+		
+		for (let i = 0; i < arr.length; i++) {
+			let tempVal = findOne(arr[i]);
+			
+			if (tempVal.length != 0) {
+				res.push(tempVal)
+			}
+		}
+		
+		console.log(res);
+		res = checkResult(res);
+		console.log(res);
+		
+		return res;
+	} else {
+		return false;
+	}
+	
 }
 
-// module.exports = {
-//     firstName: 'Anton',
-//     secondName: 'Romankov',
-//     task: getFriendlyNumbers
-// }
+findFriendly.addEventListener('click', () => {
+	checkResult(getFriendlyNumbers(start, end));
+});
 
-function smthHARD(number) {
-    let x = 0;
-    for (let i = 0; i < number; i++) {
-        if (number % i === 0) {
-            x += i;
-        }
-    }
-    return x;
+function createArray(a, b) {
+	let arr = [];
+	
+	for (let i = a; i <= b; i++) {
+		arr.push(i);
+	}
+	return arr;
 }
 
-console.log(smthHARD(220));
+function findOne(a) {
+	let sum = 0,
+		tempVal = 0,
+		tempArr = [];
+	
+	for (let i = 0; i < a; i++) {
+		if (a % i == 0) {
+			sum += i;
+		}
+	}
+	for (let j = 0; j < sum; j++) {
+		if (sum % j == 0) {
+			tempVal += j;
+		}
+	}
+	if (a != sum) {
+		if (a == tempVal) {
+			tempArr.push(a);
+			tempArr.push(sum);
+		}
+	}
+	tempArr.sort();
+	return tempArr;
+}
+
+function validation(a, b) {
+	let isValid = true;
+	
+	a = startInput.value;
+	b = endInput.value;
+	
+	if (isNaN(a) || isNaN(b) || (a == '') || (b == '') || a > b || a < 1 || b < 1) {
+		isValid = false;
+	}
+	return isValid;
+}
+
+function checkResult(arr) {
+	let result = [];
+	
+	nextInput:
+		for (let i = 0; i < arr.length; i++) {
+			let str = arr[i]; // для каждого элемента
+			for (let j = 0; j < result.length; j++) { // ищем, был ли он уже?
+				if (result[j] == str) continue nextInput; // если да, то следующий
+			}
+			result.push(str);
+		}
+	
+	return result;
+}
