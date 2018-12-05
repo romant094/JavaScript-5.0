117dbd589ce682749c6f5f2788296ff27c93cf2b
Anton Romankov <ar@asque.me>
2018-12-05 10:50:10 +0300

    Object output on the page

--- extra/lesson_11/challenge/js/script.js
+++ extra/lesson_11/challenge/js/script.js
@@ -1,4 +1,5 @@
-let request = new XMLHttpRequest;
+let request = new XMLHttpRequest,
+	cont = document.querySelector('.container');
 
 request.open('GET', 'js/cars.json');
 request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
@@ -6,10 +7,9 @@ request.send();
 
 request.addEventListener('readystatechange', () => {
     if (request.readyState == 4 && request.status == 200) {
-        data = JSON.parse(request.response);
-    } else {
-        document.write('Нет данных для отображения...');
+        let data = JSON.parse(request.response);
+	    data.cars.forEach((item) => {
+		    cont.innerHTML += `<p>${item.category}</p><p>${item.description}<p><p><img src="${item.img}"><p><p>${item.name}<p><p>${item.price}<p><hr>`;
+	    });
     }
 });
-
-console.log(data);
