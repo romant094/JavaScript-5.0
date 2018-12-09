6f3d086e2bb8355fe1f085e523867a6041be9f75
Anton Romankov <romant094@gmail.com>
2018-12-10 00:43:10 +0300

    Trying to rewrite code using promises

--- extra/lesson_12/js/script.js
+++ extra/lesson_12/js/script.js
@@ -1,33 +1,58 @@
+'use strict';
 
 let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
-inputRub.addEventListener('input', function () {
+// inputRub.addEventListener('input', () => {
+//     let request = new XMLHttpRequest();
+
+//     request.open('GET', 'js/current.json');
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send();
+
+//     request.addEventListener('readystatechange', function () {
+//         if (request.readyState === 4 && request.status == 200) {
+//             let data = JSON.parse(request.response);
+
+//             inputUsd.value = inputRub.value / data.usd;
+//         } else {
+//             inputUsd.value = "Что-то пошло не так!";
+//         }
+//     });
+// });
+
+inputRub.addEventListener('input', () => {
     function countUsd() {
-        return new Promise(function (resolve, reject) {
+        return new Promise((resolve, reject) => {
             let request = new XMLHttpRequest();
+
             request.open('GET', 'js/current.json');
             request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
             request.send();
 
-            request.addEventListener('readystatechange', () => {
-                let data = JSON.parse(request.response);
-
-                if (request.readyState === 4 && request.status == 200) {                    
-                    resolve(data);
+            request.addEventListener('readystatechange', function () {
+                if (request.readyState < 4) {
+                    resolve();
+                } else if (request.readyState === 4) {
+                    if (request.status == 200 && request.status < 300) {
+                        let rates = JSON.parse(request.response);
+                        resolve(rates);
+                    }
                 } else {
-                    reject();
+                    let error = "Что-то пошло не так!";
+                    reject(error);
                 }
-            })
+            });
         });
     }
 
+    // console.log(countUsd());
     countUsd()
         .then(() => {
-            inputUsd.value = inputUsd.value / data.usd;
+            console.log(rates);
+            inputUsd.value = inputRub.value / rates.usd;
         })
-        .catch(() => {
-            inputUsd.value = "Что-то пошло не так!";
+        .catch((error) => {
+            inputUsd.value = error;
         });
 });
-
